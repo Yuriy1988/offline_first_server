bfc476fdd81975eb98b621a119998fc818a45ad0
Yuriy1988 <csnmetau@gmail.com>
2018-03-04 20:24:32 +0200

    hz

--- src/server/index.js
+++ src/server/index.js
@@ -6,21 +6,4 @@ module.exports = {
     router: require('./router'),
     rewriter: require('./rewriter'),
     bodyParser: require('./body-parser')
-}
-
-var data = {
-    movies: [{
-        id: 1,
-        actors: [1]
-    }],
-    actors: [{
-        id: 1,
-        name: 'BIba',
-    }]
 };
-
-data.movies.map(m => {
-  var calculatedActorsIds = m.actors.map(aName => data.actors.find(a => a.name === aName).id);
-
-  return { ...m, actors: calculatedActorsIds}
-})
